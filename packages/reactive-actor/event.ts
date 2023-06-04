795eec2971fe9e758ef8297fa82f2433b9377880
R3D_C⊙DE <132403550+R3D-CODE@users.noreply.github.com>
2023-06-05 00:57:51 +0300

    Create event.ts

---
+++ packages/reactive-actor/event.ts
@@ -0,0 +1,4 @@
+// Everything related to event
+// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget
+// https://developer.mozilla.org/en-US/docs/Web/API/Event
+// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent
