8e5edaa4e1cf365f2ec58d1adf488506625679bb
R3D_C⊙DE <132403550+R3D-CODE@users.noreply.github.com>
2023-06-05 16:10:07 +0300

    Update hub-actor.ts

--- packages/reactive-actor/hub-actor.ts
+++ packages/reactive-actor/hub-actor.ts
@@ -1,23 +1,26 @@
 // Hub is Mediator-Observable
 // Actor is Observer
 
-// Event: Events can be used as a communication mechanism between the Hub and its Actors. 
-// The Hub can trigger events to notify Actors, and Actors can add (subscribe) to these events 
-// to receive updates. Events provide a decoupled and flexible way of propagating changes.
+// Event: Events can be used as a communication mechanism between the Hub 
+// and its Actors. The Hub can trigger events to notify Actors, and Actors 
+// can subscribe/unsubscribe to/from these events to receive updates. 
+// Events provide a decoupled and flexible way of propagating changes.
 
-// Data: Data represents the information that is being observed. It can be a single value, 
-// a collection of values, or a complex data structure. Actors can access and consume this 
-// data when they receive updates.
+// Data: Data represents the information that is being observed. It can be 
+// a single value, a collection of values, or a complex data structure. 
+// Actors can access and consume this data when they receive updates.
 
-// Callback: A callback mechanism provides a way to handle scenarios where an Actor is not present 
-// or cannot receive updates. It ensures that data changes are handled appropriately even in 
-// the absence of certain Actors. This can be achieved through default values, error handling, 
-// or alternative update strategies. Callbacks can also be used to perform
-// additional operations or specific operations when an event is triggered.
+// Callback: A callback mechanism provides a way to handle scenarios where an 
+// Actor is not present or cannot receive updates. It ensures that data changes 
+// are handled appropriately even in the absence of certain Actors. This can be 
+// achieved through default values, error handling, or alternative update strategies. 
+// Callbacks can also be used to perform additional operations or specific operations 
+// when an event is triggered.
 
-// The Hub (mediator-observable) is the source of data or state changes and the hub 
-// for controlling communications. It maintains a list of actors and notifies them when 
-// changes occur. The Hub can expose methods for actors to add (subscribe) and remove (unsubscribe).
+// The Hub (mediator-observable) is the source of event and data or state changes and 
+// the center for controlling communications. It maintains a list of actors and notifies 
+// them when changes occur. The Hub can expose methods for actors to subscribe and unsubscribe. 
+// An app can have multiple hubs.
 export interface Hub {
   addActor(actor: Actor): void;
   removeActor(actor: Actor): void;
@@ -25,8 +28,8 @@ export interface Hub {
 }
 
 // The Actor (observer) represents the entities that are interested in receiving updates 
-// or notifications about changes in the Hub (mediator-observable). Hub can be functions, objects, 
-// or components that subscribe/unsubscribe to/from the Hub.
+// or notifications about changes in the Hub (mediator-observable). Actors can be functions, 
+// objects, or components that subscribe/unsubscribe to/from the Hub.
 export interface Actor {
   subscribe(hub: Hub): void;
   unsubscribe(hub: Hub): void;
