3e543b774e4060160cdff3e67d2ff2c94d055609
R3D_C⊙DE <132403550+R3D-CODE@users.noreply.github.com>
2023-06-06 02:49:35 +0300

    Update event.ts

--- packages/reactive-actor/event.ts
+++ packages/reactive-actor/event.ts
@@ -2,3 +2,7 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget
 // https://developer.mozilla.org/en-US/docs/Web/API/Event
 // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent
+
+// Common Signals: onEvent(signal: Signal, slot: () => void): void; 
+// Our way: onEvent(signal: Signal, data?: Data, slot?: () => void): void;
+// Our way: notifyActors(event: Singal, data?: Data, callback?: () => void | Promise<void>): void | Promise<void>;
